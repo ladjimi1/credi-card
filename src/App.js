e86a804023d88ab7a1bdd994dc930745add6edb8
ladjimi1 <faroukladjimi@gmail.com>
2019-12-10 10:55:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import puce from './puce.jpg'
+import visa from './visa.jpg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 class="titre ">CREDIT CARD</h1>
+      <img className="puce" src={puce}></img>
+
+      <div className="container">
+
+        <div className="container-2">
+
+          <p className="num">7253  3256  7895  1245</p>
+          <p className="date">MONTH/YEAR</p>
+
+          <div className="l-2-horizontal">
+            <p className="date-2">5422</p>
+
+            <div className="box-right">
+              <p className="validation">VALID<br />THRU</p>
+              <i class="fas fa-caret-right" ></i>
+              <p className="date-2">11/50</p>
+            </div>
+
+          </div>
+
+          <p className="sous-titre">CARDHOLDER</p>
+
+        </div>
+
+        <img className="visa" src={visa} ></img>
+
+      </div>
+
     </div>
   );
 }
